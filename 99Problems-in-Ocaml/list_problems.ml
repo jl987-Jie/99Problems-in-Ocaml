6fccc7064a8e5ad9ea9bc02ce2fc7826d9e9ed3f
jl987 <jl987@cornell.edu>
2014-01-17 22:57:14 -0500

    aded list problem solutions

---
+++ 99Problems-in-Ocaml/list_problems.ml
@@ -0,0 +1,23 @@
+let (@) xs ys = List.rev_append (List.rev xs) ys
+
+(* 
+ *  Write a function 
+ * map : ('a -> 'b) -> 'a list -> 'b list 
+ * that is an implementation of the List.map 
+ * function using only List.fold_left or List.fold_right.
+ *)
+let map f lst =
+    let h accu elm =
+        accu @ [f elm] in
+    List.fold_left h [] lst
+
+(* Write a function 
+ * last : 'a list -> 'a option 
+ * that returns the last element of a list. *)
+let rec last lst =
+    match lst with
+    | [] -> None
+    | hd :: [] -> Some hd
+    | hd :: tl -> last tl
+
+
