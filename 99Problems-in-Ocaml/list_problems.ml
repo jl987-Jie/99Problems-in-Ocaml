fd5763a1ab934576eef4837e47d634ae7f6cf792
jie <jl987@cornell.edu>
2014-07-18 22:55:37 -0400

    added up to split function

--- 99Problems-in-Ocaml/list_problems.ml
+++ 99Problems-in-Ocaml/list_problems.ml
@@ -160,3 +160,104 @@ let encode lst =
                 (elm, accu @ [(count + 1, prev)], [elm], cur_pos + 1, 0) in
         match List.fold_left h (hd, [], [], 0, -1) lst with
         | (_, s, _, _, _) -> s
+
+(* 
+ Modify the result of the previous problem in such a way that if an element 
+ has no duplicates it is simply copied into the result list. Only elements with 
+ duplicates are transferred as (N E) lists.
+*)
+
+type 'a rle = | One of 'a | Many of int * 'a
+
+(* modified_encoding ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+- : string rle list =
+[Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+ Many (4, "e")] 
+ *)
+
+let modified_encoding lst = 
+    (* use the encode function and check if the count is 1 or many *)
+    let encoded_lst = encode lst in
+    let h accu elm = 
+        match elm with
+        | (count, c) -> 
+            if count = 1 then
+                accu @ [One c]
+            else
+                accu @ [Many (count, c)] in
+    List.fold_left h [] encoded_lst
+
+(* Decode a run-length encoded list. (medium)
+ Given a run-length code list generated as specified in the previous problem, 
+ construct its uncompressed version.
+# decode [Many (4,"a"); One "b"; Many (2,"c"); Many (2,"a"); One "d"; Many (4,"e")];;
+- : string list =
+["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] *)
+
+(* return a list containing c #count times *)
+let rec dupl count c = 
+    if count = 0 then [] else c :: dupl (count - 1) c
+
+let decode lst =
+    let h accu elm =
+        match elm with 
+        | One e -> accu @ [e]
+        | Many (count, e) -> accu @ dupl count e in
+    List.fold_left h [] lst
+
+(* Duplicate the elements of a list. (easy) *)
+(* # duplicate ["a";"b";"c";"c";"d"];;
+- : string list = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"] *)
+let duplicate lst = 
+    let h accu elm =
+        accu @ [elm] @ [elm] in
+    List.fold_left h [] lst
+
+(* Replicate the elements of a list a given number of times. (medium) 
+# replicate ["a";"b";"c"] 3;;
+- : string list = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
+ *)
+let replicate lst count = 
+    let h accu elm =
+        accu @ dupl count elm in
+    List.fold_left h [] lst
+
+(* Drop every N'th element from a list. (medium) 
+# drop_nth_elm ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3;;
+- : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
+ *)
+
+let drop_nth_elm lst n = 
+    let h (accu, count) elm =
+        if count = n then
+            (accu, 1)
+        else
+            (accu @ [elm], count + 1) in
+    fst(List.fold_left h ([], 1) lst)
+
+(* Split a list into two parts; the length of the first part is given. (easy)
+If the length of the first part is longer than the entire list, then the 
+first part is the list and the second part is empty. 
+
+# split ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3;;
+- : string list * string list =
+(["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"])
+# split ["a";"b";"c";"d"] 5;;
+- : string list * string list = (["a"; "b"; "c"; "d"], []) *)
+
+let split lst n =
+    let len = List.length lst in
+    if n >= len then
+        (lst, [])
+    else
+    (* keep a first part and second part and when count reaches n, append
+        to the second list *)
+        let h (fst_part, snd_part, count) elm =
+            if count <= n then
+                (fst_part @ [elm], snd_part, count + 1)
+            else
+                (fst_part, snd_part @[elm], count + 1) in
+        let result = List.fold_left h ([], [], 1) lst in
+        match result with
+        | (f, s, _) -> (f, s)
+
